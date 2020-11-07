fe52275d46fcd8ea525681052a9d6512fe070298
ABRAHAM-1998 <abrahamjolly3377@gmail.com>
2020-11-07 15:07:17 +0530

    login Api

--- register and login route.js
+++ register and login route.js
@@ -7,24 +7,33 @@ const { ObjectId } = require("mongodb");
 
 
 regloginRoute.login = (req, res) => {
-    console.log(req.body)
-    res.json({ status: true, message: 'succesfully deleted' })
+    db.getDB().collection('UserAccounts').findOne({ name: req.body.name, password: req.body.password }, { projection: { phone: 0, name: 0, dob: 0, email: 0, password: 0 } }, (err, result) => {
+        if (err)
+            throw err;
+        else if (result == null) {
+            res.json({ status: false, message: 'INVALID_USER' })
+        } else {
+            res.json({ status: true, message: ' Login SUCCESS', data:result._id})
+            console.log(result)
+        }
+
+    })
 }
 
 regloginRoute.reguser = (req, res) => {
-   var name = req.body.name
+    var name = req.body.name
     var email = req.body.email
-    var password  = req.body.password
+    var password = req.body.password
     var phone = req.body.phone
 
-    
-    if (name !== ''&& email !== ''&& password !== ''&& phone !== ''){
+
+    if (name !== '' && email !== '' && password !== '' && phone !== '') {
         db.getDB().collection('UserAccounts').insertOne(req.body, (err, result) => {
             if (err) throw err
             res.json({ status: true, message: 'succesfully Registered' })
         })
     }
-    else{
+    else {
         res.json({ status: false, message: 'Registration failed' })
     }
 }
