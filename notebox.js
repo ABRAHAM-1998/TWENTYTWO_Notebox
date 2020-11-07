8445d26d69c0e98eadfc88fb291752f30983ee54
ABRAHAM-1998 <abrahamjolly3377@gmail.com>
2020-11-07 13:19:30 +0530

    test

--- notebox.js
+++ notebox.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require("body-parser");
+const db = require('./database/dbmongo');
+
 var cors = require('cors')
 const app = express();
 
@@ -13,11 +15,25 @@ app.use(cors())
 
 const regloginRoute = require('./register and login route');
 app.use(require('./register and login route'));
-
+;
 app.post("/login",regloginRoute.login)
+app.post("/reguser",regloginRoute.reguser);
 
-
+//PORT
 const PORT = process.env.PORT || 4200;
 const server = app.listen(PORT, console.log('PORT RUNNING ON ::::::4200 :::::'))
+
+//DATABASE CONNNECTION
+db.connect((err) => {
+    if (err) {
+      console.log('unable to connect to database');
+      process.exit(1);
+    }
+  
+    else {
+  
+      console.log('CONNECTED TO MONGO :::27017::::');
+    }
+  });
 
 module.exports = { server }
--- register and login route.js
+++ register and login route.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const regloginRoute = express.Router();
+const db = require('./database/dbmongo')
+const { ObjectId } = require("mongodb");
+
+
 
 
 regloginRoute.login = (req, res) => {
     console.log(req.body)
+    db.getDB().collection('logindata').insertOne(req.body)
+
     res.send(req.body)
     
+}
+
+regloginRoute.reguser = (req,res) =>{
+    console,log(req.body.name)
+    res.json({ status: true, message: 'succesfully deleted' })
 }
 
 
